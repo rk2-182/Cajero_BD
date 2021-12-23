9a7fe416f46b9a76c1b1438167fc573d3c5c20a6
rk2-182 <ricardoplus@live.cl>
2021-12-23 11:56:19 -0300

    se agrega el metodo ingresar monto a la bd

--- cliente.py
+++ cliente.py
@@ -21,9 +21,10 @@ class Cliente:
 
     def iniciar_sesion(self):
         print("\n********** Banco Cripto $$$ $$$ $$$ **********")
-        sql = "select * from usuarios_banco where rut = %s"
+
+        sql = "select * from usuarios_banco where rut = %s" #consulta sql para buscar usuario
         usuario = input("Ingrese el rut para iniciar sesion de usuario: ")
-        self.rut = usuario
+        self.rut = usuario #tomar registro
         values=(usuario,)
         #resultado = usuario in self.usuarios
 
@@ -49,17 +50,28 @@ class Cliente:
         print("Fecha: {0}".format(self.fecha))
         print("********** Banco Cripto $$$ $$$ $$$ **********")
         print("")
-        if self.busqueda == 1:
-            print("Usuario encontrado:")
-            dinero = float(input("Ingrese el monto: $"))
+        
+        #sql = "insert into usuarios_banco (monto_total,rut) values (%s)"
+        sql = "update usuarios_banco set monto_total = monto_total + %s where rut=%s;"
 
-            if dinero >0:
-                print("El usuario {0} a ingresado un monto de {1} con exito!".format(self.usuario_session,dinero))
-                sleep(1)
-                self.monto_total = dinero + self.monto_total
+        rut = self.rut #asignar rut de la session 
+        print(self.rut)
+        dinero = float(input("Ingrese el monto: "))
+        values = (dinero,rut)
+
+        if dinero > 0:
+            print("El usuario {0} a ingresado un monto de {1} con exito!".format(rut,dinero))
+            sleep(1)
+            self.monto_total = dinero + self.monto_total
+            cursor.execute(sql,values)
+            bd.commit()
+            #Comprobar exito de la operacion
+            if cursor.rowcount == 1:
+                print("Registro ingresado exitosamente")
+            else:
+                print("Ocurrio un error al ingresar")
         else:
-            print("Usuario no encontrado!")
-
+            print("No puede ingresar un monto igual a o menor a 0")
 
         return self.monto_total
 
